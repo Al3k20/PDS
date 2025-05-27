f5d1189bf08201ec0a55addc5677ec067f75f7c7
Al3k20 <alexandre.novaki@escola.pr.gov.br>
2025-05-27 08:22:58 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,25 +1,26 @@
 import functools
 
 class Aluno:
-
     def __init__(self, nome, nota):
-
         self.nome = nome
-
         self.nota = nota
 
     def __repr__(self):
-
         return f'{self.nome}: {self.nota}'
 
-
-
 def comparar_alunos(a, b):
-
     return (a.nota > b.nota) - (a.nota < b.nota)
 
-alunos = [Aluno('Ana', 88), Aluno('Rafael', 92), Aluno('Paulo', 78)]
+# Criando lista de alunos a partir da entrada do usuário
+alunos = []
+for _ in range(3):
+    nome = input("Digite o nome do aluno: ")
+    nota = int(input("Digite a nota do aluno: "))
+    alunos.append(Aluno(nome, nota))
 
+# Ordenando a lista de alunos
 alunos_ordenados = sorted(alunos, key=functools.cmp_to_key(comparar_alunos))
 
-print(alunos_ordenados)
+print("Alunos ordenados por nota:")
+for aluno in alunos_ordenados:
+    print(aluno)
