7163da9caf83a596e25e40a79679533022845b35
Al3k20 <alexandre.novaki@escola.pr.gov.br>
2025-06-05 11:47:39 -0300

    Create intervalo.py

---
+++ intervalo.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Gera a sequência usando range e exibe os números
+for i in range(numero + 1):  # Adiciona +1 para incluir o número fornecido
+    print(i)
