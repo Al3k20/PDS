bc87175de4a24022ae9c7426f26d20e8a97e4a23
Al3k20 <alexandre.novaki@escola.pr.gov.br>
2025-06-16 10:16:13 -0300

    Create biblioteca_ordenacao.py

---
+++ biblioteca_ordenacao.py
@@ -0,0 +1,16 @@
+[?2004l
+Ordenação por título:
+Livro: 1984, Autor: George Orwell, Ano: 1949
+Livro: Brave New World, Autor: Aldous Huxley, Ano: 1932
+Livro: The Catcher in the Rye, Autor: J.D. Salinger, Ano: 1951
+
+Ordenação por ano de publicação:
+Livro: Brave New World, Autor: Aldous Huxley, Ano: 1932
+Livro: 1984, Autor: George Orwell, Ano: 1949
+Livro: The Catcher in the Rye, Autor: J.D. Salinger, Ano: 1951
+
+Ordenação por autor:
+Livro: Brave New World, Autor: Aldous Huxley, Ano: 1932
+Livro: 1984, Autor: George Orwell, Ano: 1949
+Livro: The Catcher in the Rye, Autor: J.D. Salinger, Ano: 1951
+[?2004h
